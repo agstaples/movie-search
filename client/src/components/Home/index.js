6eb25f49e78f2653faf8187b6de0a3a8c40cfc5c
Anne Staples <annestaples@Annes-MacBook-Pro.local>
2020-03-05 16:54:14 -0800

    adds form

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
+import { Form } from '../../components/MovieSearch'
+
 class Home extends React.Component {
     render() {
         return (
             <div className="container">
-                <h1>Movie Search</h1>
+                <div className="row pt-5">
+                    <div className="col-12 col-lg-6 offset-lg-3">
+                        <h1 className="text-center">MovieSearch</h1>
+                    </div>
+                    <Form />
+                </div>
             </div>
         )
     }
