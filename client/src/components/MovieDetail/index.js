ef26f007202b6604412376c2badf858614ee3e3a
Anne Staples <annestaples@Annes-MacBook-Pro.local>
2020-03-09 11:06:37 -0700

    updates search results page

--- client/src/components/MovieDetail/index.js
+++ client/src/components/MovieDetail/index.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react'
+import NumberFormat from 'react-number-format'
 import axios from 'axios'
 
 class MovieDetail extends React.Component {
@@ -38,41 +39,46 @@ class MovieDetail extends React.Component {
   
     render() {
         const movie = this.state.displayMovie
+        const movieBudget = movie.budget > 0
+            ? movie.budget
+            : "Budget information not available"
+        const movieRevenue = movie.revenue > 0
+            ? movie.revenue
+            : "Revenue information not available"
         return (
             <Fragment>
-                <div style={{
-                        height: 'fit-content',
-                        width: 'fit-content',
-                        }}
+                <div className="jumbotron jumbotron-image shadow" style={{
+                    backgroundImage: `linear-gradient(rgba(255,255,255,0.7), rgba(255,255,255,0.7)), url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`,
+                    backgroundSize: 'cover',
+                    overflow: 'hidden',
+                    borderRadius: '0',
+                    }}
                 >
-                    <div className="jumbotron jumbotron-image shadow" style={{
-                        backgroundImage: `linear-gradient(rgba(255,255,255,0.7), rgba(255,255,255,0.7)), url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`,
-                        backgroundSize: 'cover',
-                        overflow: 'hidden',
-                        borderRadius: '0',
+                    <div style= {{
+                        padding: '80px 40px 0px'
                         }}
                     >
-                        <div style= {{
-                            padding: '80px 40px 0px'
-                            }}
-                        >
-                            <div className="mb-4 display-4 font-weight-bold text-center">
-                                {movie.title}
-                            </div>
-                            <p className="mb-4">
-                                {movie.overview}
-                            </p>
+                        <div className="mb-4 display-4 font-weight-bold text-center">
+                            {movie.title}
                         </div>
+                        <p className="mb-4">
+                            {movie.overview}
+                        </p>
                     </div>
+                </div>
+                <div className="col-12">
                     <div className="dropdown">
-                        <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <button className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style={{ margin: "10px 0"}}>
                             View Similar Movies
                         </button>
                         {this.getSimilarMoviesDropdown()}
                     </div>
-                </div>
-                <div>
-
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Runtime: {movie.runtime} min</li>
+                        <li class="list-group-item">Budget: <NumberFormat value={movieBudget} displayType={'text'} thousandSeparator={true} prefix={'$'} /></li>
+                        <li class="list-group-item">Revenue: <NumberFormat value={movieRevenue} displayType={'text'} thousandSeparator={true} prefix={'$'} /></li>
+                        <li class="list-group-item"><a href={`https://www.imdb.com/title/${movie.imdb_id}/`}>IMDB</a></li>
+                    </ul>
                 </div>
             </Fragment>
           )
